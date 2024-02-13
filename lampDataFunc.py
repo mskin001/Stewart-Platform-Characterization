c7c667a26b6909315db54109f2acc65809c6ae55
mskinner0723 <126621498+mskinner0723@users.noreply.github.com>
2024-02-13 10:53:29 -0700

    Add Description
    Added descriptions to the data processing functions and the signal_gen script.

--- lampDataFunc.py
+++ lampDataFunc.py
@@ -1,7 +1,29 @@
+# LAMP Data processing functions. This file was created to house any
+# custom functions created for the LAMP characterization data
+# processing. For notation, host data is what we send to the
+# platform. This is the demanded motion, i.e. the signal produced by
+# signal_gen.py. The control data is the responce from the platform.
+# This is what we get back as actual motion.
+
 import numpy as np
 import scipy as sp
 
 def testDataSort(hostData, controlData):
+    # Sorts the host and controller data from smallest to largest. 
+    # This function also finds the difference (i.e. residual)
+    # between the host and controller data. It returns the sorted
+    # data and the resitual for each direction. 
+    # Inputs:
+    # hostData = (MxN) array where M is the length of the test
+    #             and N is the 3x the number of DOFs in the test
+    # controlData = (MxN) array where M is the length of the test
+    #                 and N is the 3x the number of DOFs in the 
+    #                 test
+    #
+    # Outputs:
+    # hostSort = (MxN) sortedarray where M is the length of the 
+    #             test and N is the 3x the number of DOFs in the
+    #             test.
     hostSort = np.zeros(hostData.shape)
     contSort = np.zeros(controlData.shape)
     diff = np.zeros(hostData.shape)
@@ -13,6 +35,21 @@ def testDataSort(hostData, controlData):
     return hostSort, contSort, diff
 
 def freqDist(hostData, controlData):
+    # Computes the fft column wise on the host and controller data
+    # and returns the frequency spectra.
+    # Inputs:
+    # hostData = (MxN) array where M is the length of the test
+    #             and N is the 3x the number of DOFs in the test
+    # controlData = (MxN) array where M is the length of the test
+    #                 and N is the 3x the number of DOFs in the 
+    #                 test
+    # Outputs:
+    # hostSpec = (MxN) array where M is the number of elements in
+    #             the spectra and N is the number of columns in 
+    #             hostData
+    # controlSpec = (MxN) array where M is the number of elements 
+    #                in the spectra and N is the number of columns
+    #                in hostData
     hostSpec = []
     contSpec = []
     for k in range(np.shape(hostData)[1]):
